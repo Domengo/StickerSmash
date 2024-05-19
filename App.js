60ea52b270935ba427be0a9df84b0c0c188f9f31
Domengo <dominicsengo@gmail.com>
2024-05-19 08:39:15 +0300

    chore: Refactor App.js and ImageViewer.js to improve image handling and UI layout

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+/** @format */
 
-import Button from './components/Button';
-import ImageViewer from './components/ImageViewer';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
-const PlaceholderImage = require('./assets/images/background-image.png');
+import Button from "./components/Button";
+import ImageViewer from "./components/ImageViewer";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [image, setImage] = useState(null);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -15,19 +20,23 @@ export default function App() {
     });
 
     if (!result.cancelled) {
-      alert('You picked an image.');
+      setImage(result.assets[0].uri);
     } else {
-      alert('You cancelled picking an image.');
+      alert("You cancelled picking an image.");
     }
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} image={image} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
@@ -38,15 +47,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
     flex: 1,
-    paddingTop: 58
+    paddingTop: 58,
   },
   footerContainer: {
     flex: 1 / 3,
-    alignItems: 'center',
+    alignItems: "center",
   },
 });
